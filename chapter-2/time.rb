0722048c4aeb869bdad53df05dc60ebeb888e0b7
Yong Li <ibravely@gmail.com>
2013-11-22 15:41:42 +0800

    add chapter-2 exercises

---
+++ chapter-2/time.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+puts "There're #{24 * 365} hours in a year, or #{24 * 366} hours if in a leap year."
+
+puts "There're #{10 * 365 * 24 * 60} minutes in a decade, or #{(10 * 365 +2) * 24 * 60} minutes if it includes 2 leap years."
+
+puts "I'm #{24 * 60 * 60 * 365 * 30} seconds old."
+
+puts "Our author is #{1160000000 / (24 * 60 * 60 * 365)} years old."

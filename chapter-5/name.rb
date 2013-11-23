22b7400cc2dad96564fe8c4859b37c481b6ad4c5
Yong Li <ibravely@gmail.com>
2013-11-23 17:26:22 +0800

    add chapter-5 exercises

---
+++ chapter-5/name.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+
+puts "Hello, #{first_name.capitalize} #{last_name.capitalize}!"
+
+
---
+++ chapter-5/number.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+
+print "Please enter your favorite number: "
+favorite_number = gets.chomp.to_i
+puts "What about letting #{favorite_number + 1} as your new bigger and better favorite number?"
+
+
